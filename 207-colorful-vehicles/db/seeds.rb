7c39cbaf7b4108709b08495fc9570469ef204fbd
Lourdes Prinster <sinvita@rocketmail.com>
2014-02-11 17:13:37 -0700

    206 Colorful Vehicles - intro to relational dbs

--- 207-colorful-vehicles/db/seeds.rb
+++ 207-colorful-vehicles/db/seeds.rb
@@ -1,1 +1,9 @@
-# TODO: write this
+Vehicle.create! name: "airplane", image_filename: "plane.png"
+Vehicle.create! name: "bicycle", image_filename: "bike.png"
+Vehicle.create! name: "bus", image_filename: "bus.png"
+Vehicle.create! name: "car", image_filename: "car.png"
+
+Color.create! name: "fuschia", html_color_code: "#C73ABD"
+Color.create! name: "green", html_color_code: "#6FF29D"
+Color.create! name: "purple", html_color_code: "#6776FD"
+Color.create! name: "yellow", html_color_code: "#EBF067"

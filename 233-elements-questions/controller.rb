1e4381815c47a4e4b0a5ee83be2ce26e56193fba
Lourdes Prinster <sinvita@rocketmail.com>
2014-02-27 11:16:48 -0700

    233 Elements - all, where, first, find_by

--- 233-elements-questions/controller.rb
+++ 233-elements-questions/controller.rb
@@ -7,5 +7,13 @@ end
 post "/" do
   # TODO: fill out the @answer9 or @answer10 variables in response
   # to whatever the user entered.
+  element_name   = params["name_of_element"].capitalize
+  element_number = params["number_of_element"]
+  if element_name != ""
+    @answer9 = Element.find_by(name: element_name).number
+  end
+  if element_number != ""
+    @answer10 = Element.find_by(number: element_number).name
+  end
   halt erb(:answer_to_9_or_10)
 end

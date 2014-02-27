20bda87820a92d1ca01696060702095be87e0e92
Lourdes Prinster <sinvita@rocketmail.com>
2014-02-27 11:58:59 -0700

    236 Login controller

--- 236-login/controller.rb
+++ 236-login/controller.rb
@@ -10,6 +10,23 @@ end
 #      (Bonus: pre-fill the username that the user entered last time)
 #   3. Or redirects to /success
 
+post "/" do
+  entered_username    = params["username"]
+  entered_pin_number  = params["pin_number"]
+  user = User.find_by(username: entered_username)
+  if user == nil
+    @error = "Unknown username"
+    @old_username = entered_username
+    halt erb(:login)
+  elsif user.pin_number != entered_pin_number
+    @error        = "Wrong pin number"
+    @old_username = entered_username
+    halt erb(:login)
+  else
+    redirect "/success"
+  end
+end
+
 get "/success" do
   halt erb(:success)
 end

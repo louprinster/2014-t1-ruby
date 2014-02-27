a52a15ff25654669d5d7e9398b8523f614cb7064
Lourdes Prinster <sinvita@rocketmail.com>
2014-02-27 14:13:10 -0700

    237 Same first names - practice methods on relations and rows

--- 237-same-first-names/controller.rb
+++ 237-same-first-names/controller.rb
@@ -10,9 +10,30 @@ post "/" do
   #       their info.
   # TODO: If 2+ people have that first name, show choose.html.erb
   #       to let the user choose between the people with that first name.
+
+  entered_name = params["first_name"]
+  person_list = Person.where(first_name: entered_name)
+  count = person_list.size
+  
+  if count == 0
+    halt erb(:no_one)
+  elsif count == 1
+    @person = Person.find_by(first_name: entered_name)
+    halt erb(:number)
+  else
+    @people = person_list
+    halt erb(:choose)
+    
+  end
 end
+
 
 get "/person/:first/:last" do
   # TODO: Show the info for the person with the first and last name
   #       specified in the route.
+  first = params["first"]
+  last  = params["last"]
+  @person = Person.find_by(first_name: first, last_name: last)
+  halt erb(:number)
 end
+

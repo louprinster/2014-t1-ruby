12425a8323f68a9eef378beeffedb76a98906393
Lourdes Prinster <sinvita@rocketmail.com>
2014-02-28 14:20:24 -0700

    239 Book search - string interp. in url and search

--- 239-book-search/controller.rb
+++ 239-book-search/controller.rb
@@ -6,10 +6,38 @@ end
 
 # TODO: GET handler for routes like /year/2011, /year/2007, etc.
 
+get "/year/:year" do
+  search_year = params["year"]
+  @books = Book.where(publication_year: search_year)
+  @header = "Books by publication year #{search_year}"
+  halt erb(:search_results)
+end
+
+
 # TODO: GET handler for routes like /author/Olsen,
 #       /author/Crockford, etc.
 
+get "/author/:last_name" do
+  search_last_name = params["last_name"]
+  @books = Book.where("author ilike ?", "%#{search_last_name}")
+  @header = "Books by author #{search_last_name}"
+  halt erb(:search_results)
+end
+
 # TODO: GET handler for routes like /topic/jQuery, /topic/CSS, etc.
+
+get "/topic/:topic" do
+  search_topic = params["topic"]
+  @books = Book.where("title ilike ?", "%#{search_topic}%")
+  @header = "Books on #{search_topic}"
+  halt erb(:search_results)
+end
 
 # TODO: GET handler for routes like /isbn/1449397220, 
 #       /isbn/1449325947, etc.
+
+get "/isbn/:isbn" do
+  search_isbn = params["isbn"]
+  @book = Book.find_by(isbn: search_isbn)
+  halt erb(:show)
+end

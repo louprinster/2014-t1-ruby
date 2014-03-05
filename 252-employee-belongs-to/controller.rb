f4041e870d8f4d41acc9b73b2df2b3c2bbea675e
Lourdes Prinster <sinvita@rocketmail.com>
2014-03-05 15:04:58 -0700

    252 Employees - After belongs_to

--- 252-employee-belongs-to/controller.rb
+++ 252-employee-belongs-to/controller.rb
@@ -3,3 +3,23 @@ require './davinci-sinatra.rb'
 # TODO: Write handler for GET /
 
 # TODO: Write handler for POST /
+
+
+get "/" do
+    @employees = Employee.order(:id)
+    @new_employee = Employee.new
+    halt erb(:index)
+end
+
+post "/" do
+    @employees = Employee.order(:id)
+    @new_employee = Employee.new
+    @new_employee.first_name = params["first_name"]
+    @new_employee.last_name  = params["last_name"]
+    @new_employee.company_id = params["company_id"]
+    if @new_employee.save == true
+      redirect "/"
+    else
+      halt erb(:index)
+    end
+end

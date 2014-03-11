3e4e18bd06d3690be9c50fc7d1a10da2e01f5f91
Lourdes Prinster <sinvita@rocketmail.com>
2014-03-10 18:54:17 -0600

    253 Bank Login - sessions, cookies

--- 253-bank-login/controller.rb
+++ 253-bank-login/controller.rb
@@ -17,28 +17,79 @@ post "/login" do
   # and redirect to /accounts, or set an error message in @error
   # and re-render login.html.erb.
 
-  redirect "/accounts"
+  username = params["username"]
+  password = params["password"]
+
+  user = User.find_by(username: username)
+  
+  if user != nil
+      if password == user.password
+        session[:user_id] = user.id
+        redirect "/accounts"
+      else
+        @error = "Password not found"
+        halt erb(:login)
+      end
+  else
+    @error = "Username not found"
+    halt erb(:login)
+  end
+  
 end
 
 get "/accounts" do
-  @user = User.find(1) # TODO: Remove this line
+#  @user = User.find(1) # TODO: Remove this line
   # TODO: Load the user based on the id saved to the session
 
-  halt erb(:accounts)
+  user_id = session[:user_id]
+  
+  if user_id != nil
+    @user = User.find(user_id)
+    halt erb(:accounts)
+  else # prevent access to this URL without logging in
+    @error = "Please login"
+    halt erb(:login)
+  end
+  
 end
 
 get "/location" do
-  @user = User.find(1) # TODO: Remove this line
+#  @user = User.find(1) # TODO: Remove this line
   # TODO: Load the user based on the id saved to the session
 
-  halt erb(:location)
+  user_id = session[:user_id]
+  
+  if user_id != nil
+    @user = User.find(user_id)
+    halt erb(:location)
+  else
+    @error = "Please login"
+    halt erb(:login)
+  end
+  
 end
 
 get "/rates" do
-  @user = User.find(1) # TODO: Remove this line
+#  @user = User.find(1) # TODO: Remove this line
   # TODO: Load the user based on the id saved to the session
 
-  halt erb(:rates)
+  user_id = session[:user_id]
+  
+  if user_id != nil
+    @user = User.find(user_id)
+    halt erb(:rates)
+  else
+    @error = "Please login"
+    halt erb(:login)
+  end
+  
 end
 
 # TODO: Write handler for GET /logout that logs out the user
+
+get "/logout" do
+  session.clear
+  redirect "/login"
+end
+
+

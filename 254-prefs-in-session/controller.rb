fa6eb9d7ada95257fec04aa7a8598606585f83f6
Lourdes Prinster <sinvita@rocketmail.com>
2014-03-13 18:51:18 -0600

    254 prefs in database - sessions/cookies intro

--- 254-prefs-in-session/controller.rb
+++ 254-prefs-in-session/controller.rb
@@ -4,6 +4,10 @@ get "/" do
   # TODO: Retrieve three values stored in the session and use them
   # to pre-fill form values in the view.
 
+  @color  = session[:color]
+  @letter = session[:letter]
+  @number = session[:number]
+
   halt erb(:index)
 end
 
@@ -11,8 +15,23 @@ post "/" do
   # TODO: Transfer values of POST params to become local variables.
   # TODO: Save those local variables to the session so the values
   #       will last.
+  if params["commit"] == "Update preferences"
+    color  = params["color"]
+    letter = params["letter"]
+    number = params["number"]
+  
+    session[:color] = color
+    session[:letter] = letter
+    session[:number] = number
+    
+    redirect "/"
 
-  redirect "/"
+  end
+
 end
 
 # TODO: Write handler for GET /logout that empties the session
+get "/logout" do
+  session.clear
+  redirect "/"
+end
--- 255-prefs-in-database/controller.rb
+++ 255-prefs-in-database/controller.rb
@@ -5,6 +5,13 @@ get "/" do
   # TODO: ...then use it to load the @user instance variable.
   # TODO: Otherwise just start a new empty User.
 
+  user_id = session[:user_id]
+  
+  if user_id != nil
+    @user = User.find(user_id)
+  else
+    @user = User.new
+  end
   halt erb(:index)
 end
 
@@ -13,14 +20,35 @@ post "/" do
   # TODO: ...then use it to load the @user instance variable.
   # TODO: Otherwise just start a new empty User.
 
+  user_id = session[:user_id]
+  if user_id != nil
+    @user = User.find(user_id)
+  else
+    @user = User.new
+  end
+
   # TODO: Set the user's new preferences from POST params on the User
   # TODO: Then save the User object
+
+  @user.color = params["color"]
+  @user.letter = params["letter"]
+  @user.number = params["number"]
+  @user.save!
+  # Use save! here (instead of just save) to raise an exception if a validation fails. 
+  
 
   # TODO: Save that user ID to the session, in case we created a new
   #       one. (If we already had a user ID in the session, it doesn't
   #       hurt to overwrite it with the same value).
+  
+  session[:user_id] = @user.id
 
   redirect "/"
 end
 
 # TODO: Write handler for GET /logout
+
+get "/logout" do
+  session.clear
+  redirect "/"
+end

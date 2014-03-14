9e833fe1eb0a3b1a0c736b1175215d32916a3c95
Lourdes Prinster <sinvita@rocketmail.com>
2014-03-13 18:49:13 -0600

    256 has secure password - login session, pw authenticate

--- 256-has-secure-password/controller.rb
+++ 256-has-secure-password/controller.rb
@@ -15,6 +15,18 @@ get "/users/new" do
 end
 
 # TODO: Write POST route for handling registration form submissions
+post "/users/new" do
+  @user = User.new
+  @user.username = params["username"]
+  @user.password = params["password"]
+  @user.password_confirmation = params["password_confirmation"]
+  
+  if @user.save == true
+    redirect "/login"
+  else
+    halt erb(:new)
+  end
+end
 
 # Setup GET route for login form
 get "/login" do
@@ -22,6 +34,23 @@ get "/login" do
 end
 
 # TODO: Write POST route to handle login form submissions
+
+post "/login" do
+  @user = User.find_by(username: params["username"])
+  if @user != nil
+    attempted_password = params["password"]
+    if @user.authenticate(attempted_password) != false
+      session[:user_id] = @user.id
+      halt erb(:welcome)
+    else
+      @message = "Incorrect password"
+      halt erb(:login)
+    end
+  else
+    @message = "Username does not exist"
+    halt erb(:login)
+  end  
+end
 
 # An example page that's accessible only after you're logged in
 get "/welcome" do
@@ -35,3 +64,7 @@ get "/welcome" do
 end
 
 # TODO: Write handler for GET /logout route, to clear the session.
+get "/logout" do
+  session.clear
+  redirect "/login"
+end
